527b7f5e59437d00ba49bf700cdff92301c88da7
Elliot <elliotgould5721@gmail.com>
2024-08-25 10:32:00 +0100

    Mapping the Employee data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
 import "./index.css";
 import Employee from "./components/Employee";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [role, setRole] = useState("dev");
+  const [employees, setEmployees] = useState([
+    {
+      name: "Caleb", 
+      role: "Youtube Sensation",
+      img: "https://images.pexels.com/photos/1839564/pexels-photo-1839564.jpeg?",
+    },
+    {
+      name: "Shaziya",
+      role: "Software Engineer",
+      img: "https://images.pexels.com/photos/25756/pexels-photo.jpg?",
+    },
+    {
+      name: "Abby",
+      role: "Manager",
+      img: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?",
+    },
+    {
+      name: "Someone",
+      role: "Web-Developer",
+      img: "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?",
+    },
+    {
+      name: "John",
+      role: "HardWare Engineer",
+      img: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?",
+    },
+    {
+      name: "Nancy",
+      role: "Developer",
+      img: "https://images.pexels.com/photos/2169434/pexels-photo-2169434.jpeg?",
+    },
+    {
+      name: "Elliot",
+      role: "Developer",
+      img: "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?",
+    },
+    {
+      name: "Nile",
+      role: "Developer",
+      img: "https://images.pexels.com/photos/2287252/pexels-photo-2287252.jpeg?",
+    },
+    {
+      name: "Beauty",
+      role: "Developer",
+      img: "https://images.pexels.com/photos/2083751/pexels-photo-2083751.jpeg?",
+    },
+  ]);
   const showEmployee = true;
 
   return (
@@ -17,59 +65,21 @@ function App() {
             }}
           />
 
-          <div className="flex flex-wrap ">
-            <Employee
-              name="Caleb"
-              role="Intern"
-              img="https://images.pexels.com/photos/1839564/pexels-photo-1839564.jpeg?auto=compress&cs=tinysrgb&w=600"
-            />
-            <Employee
-              name="john"
-              role={role}
-              img="https://images.pexels.com/photos/25756/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600"
-            />
-
-            <Employee
-              name="Abby"
-              role="Intern"
-              img="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=600"
-            />
-            <Employee
-              name="Caleb"
-              role="Intern"
-              img="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=600"
-            />
-            <Employee
-              name="john"
-              role={role}
-              img="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=600"
-            />
-
-            <Employee
-              name="Abby"
-              role="Intern"
-              img="https://images.pexels.com/photos/2169434/pexels-photo-2169434.jpeg?auto=compress&cs=tinysrgb&w=600"
-            />
-            <Employee
-              name="Caleb"
-              role="Intern"
-              img="https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=600"
-            />
-            <Employee
-              name="john"
-              role={role}
-              img="https://images.pexels.com/photos/2287252/pexels-photo-2287252.jpeg?auto=compress&cs=tinysrgb&w=600"
-            />
-
-            <Employee
-              name="Abby"
-              role="Intern"
-              img="https://images.pexels.com/photos/2083751/pexels-photo-2083751.jpeg?auto=compress&cs=tinysrgb&w=600"
-            />
+          <div className="flex flex-wrap justify-center">
+            {employees.map((employee) => {
+              return (
+                <Employee
+                  key={uuidv4()}
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img}
+                />
+              );
+            })}
           </div>
         </>
       ) : (
-        <p>You cannot seee the Employee</p>
+        <p>You cannot see the Employee</p>
       )}
     </div>
   );
