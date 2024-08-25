fe255e1d150b3459880e8e287e87ca6969c87c65
Elliot <elliotgould5721@gmail.com>
2024-08-25 09:58:22 +0100

    Styled react with tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,32 +1,72 @@
 import "./index.css";
 import Employee from "./components/Employee";
-import { useState } from 'react';
+import { useState } from "react";
 
 function App() {
-  const [role, setRole] = useState('dev');
-  const showEmployee = true;  
-  
+  const [role, setRole] = useState("dev");
+  const showEmployee = true;
+
   return (
-    <div className="App bg-red-300">
+    <div className="App ">
       {showEmployee ? (
         <>
+          <input
+            type="text"
+            onChange={(e) => {
+              setRole(e.target.value);
+            }}
+          />
 
-        <input type="text" 
-        onChange={(e) => {
-          setRole(e.target.value)
-        }} />
-          <Employee 
-          name="Caleb" 
-          
-          role="Intern" />
-          <Employee 
-          name="john"
-          role = {role}
-           />
-          <Employee 
-           name="Abby"
-           role="Intern" />
-          
+          <div className="flex flex-wrap ">
+            <Employee
+              name="Caleb"
+              role="Intern"
+              img="https://images.pexels.com/photos/1839564/pexels-photo-1839564.jpeg?auto=compress&cs=tinysrgb&w=600"
+            />
+            <Employee
+              name="john"
+              role={role}
+              img="https://images.pexels.com/photos/25756/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600"
+            />
+
+            <Employee
+              name="Abby"
+              role="Intern"
+              img="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=600"
+            />
+            <Employee
+              name="Caleb"
+              role="Intern"
+              img="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=600"
+            />
+            <Employee
+              name="john"
+              role={role}
+              img="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=600"
+            />
+
+            <Employee
+              name="Abby"
+              role="Intern"
+              img="https://images.pexels.com/photos/2169434/pexels-photo-2169434.jpeg?auto=compress&cs=tinysrgb&w=600"
+            />
+            <Employee
+              name="Caleb"
+              role="Intern"
+              img="https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=600"
+            />
+            <Employee
+              name="john"
+              role={role}
+              img="https://images.pexels.com/photos/2287252/pexels-photo-2287252.jpeg?auto=compress&cs=tinysrgb&w=600"
+            />
+
+            <Employee
+              name="Abby"
+              role="Intern"
+              img="https://images.pexels.com/photos/2083751/pexels-photo-2083751.jpeg?auto=compress&cs=tinysrgb&w=600"
+            />
+          </div>
         </>
       ) : (
         <p>You cannot seee the Employee</p>
