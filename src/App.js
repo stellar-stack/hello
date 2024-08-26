705e23eed53980f0be6e7c0de6937111561e266b
Elliot <elliotgould5721@gmail.com>
2024-08-26 08:55:26 +0100

    Used routing with react router

--- src/App.js
+++ src/App.js
@@ -6,11 +6,19 @@ import AddEmployee from "./components/AddEmployee";
 import EditEmployee from "./components/EditEmployee";
 import Header from "./components/Header";
 import Employees from "./pages/Employees";
+import Customers from "./pages/Customers";
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <Header>
-      <Employees />
+      <BrowserRouter>
+        <Routes> 
+          <Route path = '/employees' element= {<Employees />} />
+
+          <Route path = '/customers' element= {<Customers />} />
+        </Routes>
+      </BrowserRouter>
     </Header>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuIt
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
+  { name: 'Employees', href: '/Employees', current: true },
+  { name: 'Customers', href: '/Customers', current: false },
   { name: 'Projects', href: '#', current: false },
   { name: 'Calendar', href: '#', current: false },
 ]
