f24dc990bdf437d1368fc3b02bd5eefbdaa0f1a5
Elliot <elliotgould5721@gmail.com>
2024-08-26 08:44:44 +0100

    Pages and props.children

--- src/App.js
+++ src/App.js
@@ -5,128 +5,13 @@ import { v4 as uuidv4 } from "uuid";
 import AddEmployee from "./components/AddEmployee";
 import EditEmployee from "./components/EditEmployee";
 import Header from "./components/Header";
+import Employees from "./pages/Employees";
 
 function App() {
-  const [role, setRole] = useState("dev");
-  const [employees, setEmployees] = useState([
-    {
-      id: 1,
-      name: "Caleb",
-      role: "Youtube Sensation",
-      img: "https://images.pexels.com/photos/1839564/pexels-photo-1839564.jpeg?",
-    },
-    {
-      id: 2,
-      name: "Shaziya",
-      role: "Software Engineer",
-      img: "https://images.pexels.com/photos/25756/pexels-photo.jpg?",
-    },
-    {
-      id: 3,
-      name: "Abby",
-      role: "Manager",
-      img: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?",
-    },
-    {
-      id: 4,
-      name: "Someone",
-      role: "Web-Developer",
-      img: "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?",
-    },
-    {
-      id: 5,
-      name: "John",
-      role: "HardWare Engineer",
-      img: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?",
-    },
-    {
-      id: 6,
-      name: "Nancy",
-      role: "Developer",
-      img: "https://images.pexels.com/photos/2169434/pexels-photo-2169434.jpeg?",
-    },
-    {
-      id: 7,
-      name: "Elliot",
-      role: "Developer",
-      img: "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?",
-    },
-    {
-      id: 8,
-      name: "Nile",
-      role: "Developer",
-      img: "https://images.pexels.com/photos/2287252/pexels-photo-2287252.jpeg?",
-    },
-    {
-      id: 9,
-      name: "Beauty",
-      role: "Developer",
-      img: "https://images.pexels.com/photos/2083751/pexels-photo-2083751.jpeg?",
-    },
-  ]);
-
-  function updateEmployee(id, newName, newRole) {
-    const updatedEmployees = employees.map((employee) => {
-      if (id == employee.id) {
-        return { ...employee, name: newName, role: newRole };
-      }
-      return employee;
-    });
-
-    setEmployees(updatedEmployees);
-  }
-
-
-  function NewEmployee(name, role, img) {
-
-   const newEmployee = {
-      id: uuidv4(),
-      name: name,
-      role: role,
-      img: img,
-    }
-    setEmployees([...employees, newEmployee])
-  }
-
-  const showEmployee = true;
-
   return (
-    <div className="App bg-slate-900 min-h-screen">
-      <Header />
-      {showEmployee ? (
-        <>
-           <div className="flex flex-wrap justify-center my-4">
-            {employees.map((employee) => {
-
-              const editEmployee = <EditEmployee 
-              
-              id={employee.id}
-              name={employee.name}
-              role={employee.role}
-              updateEmployee={updateEmployee}
-            />;
-              return (
-                <Employee
-                  key={employee.id}
-                  id={employee.id}
-                  name={employee.name}
-                  role={employee.role}
-                  img={employee.img}
-                  editEmployee={editEmployee}
-                />
-              );
-            })}
-          </div>
-
-          <AddEmployee newEmployee = {NewEmployee}/>
-
-        </>
-      ) : (
-        <p>You cannot see the Employee</p>
-      )}
-    </div>
-
-    
+    <Header>
+      <Employees />
+    </Header>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Header() {
+export default function Header(props) {
   return (
     <Disclosure as="nav" className="bg-gray-800">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -115,6 +115,7 @@ export default function Header() {
           ))}
         </div>
       </DisclosurePanel>
+      {props.children}
     </Disclosure>
   )
 }
